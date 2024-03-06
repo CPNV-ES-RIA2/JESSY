94a8886c1765513bee6bbd8bb7173c713f169a67
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-06 11:08:12 +0100

    fix: tests logic

--- tests/e2e/specs/button.js
+++ tests/e2e/specs/button.js
@@ -10,18 +10,22 @@ Given("The form is loaded", function (){
   cy.get("form").should("be.visible")
 });
 
-Given("I drag an drop a file", function (){
-  GetButton().should("be.disabled")
+When("I drag an drop a file", function (){
+  cy.get("input[type=file]").selectFile("tests/e2e/specs/example.jpg", {
+    action: "drag-drop"
+  })
+  cy.get("form").submit()
 });
 
 When("Check GUI language", function () {
-  ClickButton()
+  cy.get("#app > :nth-child(1) > .v-input > .v-input__control > .v-field").click()
+  cy.get(".v-list").contains("English").click()
 });
 
 Then("The result text area will show informations", function (){
-  GetArea()
+  cy.get("#input-6").should("not.be.empty")
 });
 
 Then("Language is updated", function (){
-
+  cy.get(".v-card-title").contains("Image analyser")
 });
