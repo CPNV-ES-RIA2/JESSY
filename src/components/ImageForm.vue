d72b759a02bd8b052f46d2b0b780ca991b4b618f
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-12 16:34:11 +0100

    feat: added mock intelligence

--- src/components/ImageForm.vue
+++ src/components/ImageForm.vue
@@ -2,12 +2,48 @@
   <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
     <template v-slot:title>{{ $t("$vuetify.imageText") }}</template>
     <v-card-text>
-      <v-form v-model="valid">
-        <v-file-input prepend-icon="" label="File input"></v-file-input>
-        <v-btn type="submit" block class="mt-2" disabled>Analyse</v-btn>
+      <v-form validate-on="submit lazy" @submit.prevent="analyze">
+        <v-file-input
+          v-model="file"
+          prepend-icon=""
+          label="File input"
+          @update:model-value="enableButton()"
+        ></v-file-input>
+        <v-btn type="submit" block class="mt-2" :disabled="isButtonDisabled">
+          Analyse
+        </v-btn>
         <p class="my-10">Result</p>
-        <v-textarea label="Label" disabled></v-textarea>
+        <v-textarea :disabled="isTextDisabled" :value="text"></v-textarea>
       </v-form>
     </v-card-text>
   </v-card>
 </template>
+
+<script>
+import Analyzer from "../api/mock/index.js";
+
+export default {
+  data() {
+    return {
+      file: "",
+      isButtonDisabled: true,
+      isTextDisabled: true,
+      text: "",
+    };
+  },
+  methods: {
+    enableButton() {
+      // Enable the button when a certain condition is met
+      this.isButtonDisabled = false;
+    },
+    async analyze() {
+      Analyzer.analyze().then((data) => {
+        // Handle the analysis result here
+        this.isTextDisabled = false;
+        this.text = JSON.stringify(data);
+        console.log("Analysis result:", data);
+      });
+    },
+  },
+};
+</script>
