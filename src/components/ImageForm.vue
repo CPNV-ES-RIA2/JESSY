799e51337c518801e997d7d44ad4de99c8aaa0cc
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-20 11:22:33 +0100

    feat: using axios

--- src/components/ImageForm.vue
+++ src/components/ImageForm.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
     <template v-slot:title>{{ $t("$vuetify.imageText") }}</template>
     <v-card-text>
-      <v-form validate-on="submit lazy" @submit.prevent="analyze">
+      <v-form validate-on="submit lazy" @submit.prevent="upload">
         <v-file-input
           v-model="formDatas.file"
           prepend-icon=""
@@ -33,12 +33,14 @@
 
 <script>
 import Analyzer from "../api/mock/index.js";
+import axios from "axios";
 
 export default {
   data() {
     return {
       formDatas: {
         file: null,
+        remoteFullPath: "",
         maxLabels: "",
         minConfidenceLevel: "",
       },
@@ -52,7 +54,7 @@ export default {
       // Enable the button when a certain condition is met
       this.isButtonDisabled = false;
     },
-    async analyze() {
+    async upload() {
       try {
         const formData = new FormData();
 
@@ -60,28 +62,43 @@ export default {
         formData.append("formFile", this.formDatas.file[0]);
 
         // Create headers object with the appropriate Content-Type header including boundary parameter
-        const headers = new Headers();
-        headers.append(
-          "Content-Type",
-          "multipart/form-data; boundary=---------------------------boundary"
-        );
-        // Create requestOptions object with method, headers, and body
-        const requestOptions = {
-          method: "POST",
-          headers: headers,
-          body: formData,
-        };
 
-        fetch("https://localhost:32770/api/Gateway/Upload", requestOptions)
-          .then((response) => response.json())
-          .then((data) => (this.postId = data.id));
+        axios
+          .post("https://localhost:32780/api/Gateway/Upload", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            this.formDatas.remoteFullPath = response.data;
+            this.analyze();
+          })
+          .catch((response) => {
+            this.handleError(response);
+          });
       } catch (error) {
-        Analyzer.analyze().then((data) => {
-          // Handle the analysis result here
-          this.isTextDisabled = false;
-          this.text = JSON.stringify(data);
-        });
+        this.handleError(error);
       }
+    },
+    handleError(response) {
+      console.log(response);
+      Analyzer.analyze().then((data) => {
+        // Handle the analysis result here
+        this.isTextDisabled = false;
+        this.text = JSON.stringify(data);
+      });
+    },
+    analyze() {
+      axios
+        .post("https://localhost:32768/api/Gateway/Analyze", this.formDatas)
+        .then((response) => {
+          // Handle response
+          console.log(response.data);
+        })
+        .catch((error) => {
+          // Handle error
+          console.error(error);
+        });
     },
   },
 };
